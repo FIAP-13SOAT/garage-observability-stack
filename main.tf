404d47e3538878ef23e453d7adca2b548e228be4
Johann Bandelow <johannabandelow@gmail.com>
2026-03-24 19:28:26 -0300

    fix: terraform format files

--- main.tf
+++ main.tf
@@ -1,65 +1,65 @@
 terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "6.17.0"
-        }
-        datadog = {
-            source  = "DataDog/datadog"
-            version = "~> 3.50"
-        }
-        helm = {
-            source  = "hashicorp/helm"
-            version = "~> 2.17"
-        }
-        kubernetes = {
-            source  = "hashicorp/kubernetes"
-            version = "~> 2.36"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.17.0"
     }
+    datadog = {
+      source  = "DataDog/datadog"
+      version = "~> 3.50"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.17"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.36"
+    }
+  }
 
-    backend "s3" {
-        bucket = "garage-terraform-state-211125475874"
-        key    = "observability/terraform.tfstate"
-        region = "us-east-1"
-    }
+  backend "s3" {
+    bucket = "garage-terraform-state-211125475874"
+    key    = "observability/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 locals {
-    projectName = "garage"
-    awsRegion   = "us-east-1"
-    environment = var.environment
-    clusterName = "${local.projectName}-cluster"
+  projectName = "garage"
+  awsRegion   = "us-east-1"
+  environment = var.environment
+  clusterName = "${local.projectName}-cluster"
 }
 
 provider "aws" {
-    region = local.awsRegion
+  region = local.awsRegion
 }
 
 data "aws_eks_cluster" "cluster" {
-    name = local.clusterName
+  name = local.clusterName
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-    name = local.clusterName
+  name = local.clusterName
 }
 
 provider "kubernetes" {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "helm" {
-    kubernetes {
-        host                   = data.aws_eks_cluster.cluster.endpoint
-        cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-        token                  = data.aws_eks_cluster_auth.cluster.token
-    }
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
 }
 
 provider "datadog" {
-    api_key = var.datadog_api_key
-    app_key = var.datadog_app_key
-    api_url = "https://us5.datadoghq.com/"
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+  api_url = "https://us5.datadoghq.com/"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,20 @@
 output "datadog_agent_status" {
-    description = "Status do Helm release do Datadog Agent"
-    value       = helm_release.datadog_agent.status
+  description = "Status do Helm release do Datadog Agent"
+  value       = helm_release.datadog_agent.status
 }
 
 output "datadog_monitor_ids" {
-    description = "IDs dos monitores criados no Datadog"
-    value = {
-        service_order_error_rate = datadog_monitor.service_order_error_rate.id
-        health_check             = datadog_monitor.health_check.id
-        api_latency_p95          = datadog_monitor.api_latency_p95.id
-        memory_usage             = datadog_monitor.memory_usage.id
-        cpu_usage                = datadog_monitor.cpu_usage.id
-    }
+  description = "IDs dos monitores criados no Datadog"
+  value = {
+    service_order_error_rate = datadog_monitor.service_order_error_rate.id
+    health_check             = datadog_monitor.health_check.id
+    api_latency_p95          = datadog_monitor.api_latency_p95.id
+    memory_usage             = datadog_monitor.memory_usage.id
+    cpu_usage                = datadog_monitor.cpu_usage.id
+  }
 }
 
 output "datadog_dashboard_url" {
-    description = "URL do dashboard no Datadog"
-    value       = datadog_dashboard_json.garage_operations.url
+  description = "URL do dashboard no Datadog"
+  value       = datadog_dashboard_json.garage_operations.url
 }
